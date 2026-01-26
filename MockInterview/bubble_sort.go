5d331902b8e0a31a59123ca5dd6d4833411707af
affulk000 <affulk000@gmail.com>
2026-01-26 08:49:24 +0000

    feat(bubble_sort): add bubble sort of an array

---
+++ MockInterview/bubble_sort.go
@@ -0,0 +1,20 @@
+// Like bubbles rising: Each pass, largest sinks to end.
+// I loop n times, inner compares i to i+1 and swaps if needed.
+// Early stop if sorted
+package mockinterview
+
+func bubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		swapped := false
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = true
+			}
+		}
+		if !swapped {
+			break
+		}
+	}
+}
